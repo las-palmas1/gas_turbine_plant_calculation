3f3a1c62005640ebb7eeb98a137096262c54d296
las-palmas1 <las-palmas1@mail.ru>
2018-06-13 05:54:39 +0300

    Add cooling and profiling calculation.

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ eta_gen = 0.978
 output_dirname = 'output results'
 # имя файла для хранения результатов расчета цикла
 cycle_results = 'cycle.cyc'
+# папка для графиков
+plots_dir = 'plots'
+# папка для графиков для черетежей
+draw_plots_dir = 'plots for drawing'
